a9d5d5550e023cdde63eebe8863f5d90cb4f0d62
SergeyKoavalev <dip.deep@mail.ru>
2022-01-23 00:20:45 +0300

    Added second test

--- src/test/java/BefungeInterpreterTest.java
+++ src/test/java/BefungeInterpreterTest.java
@@ -9,6 +9,18 @@ public class BefungeInterpreterTest {
         assertEquals(
                 "123456789",
                 new BefungeInterpreter().interpret(">987v>.v\nv456<  :\n>321 ^ _@"));
-
+        assertEquals(
+                "FizzBuzz",
+                new BefungeInterpreter().interpret("0v       v    <\n" +
+                        " >91+91+*>:1-:|\n" +
+                        "              $    >\"zziF\",,,,v\n" +
+                        "              >:3%!|v         <\n" +
+                        "                         >\"zzuB\",,,,v           @\n" +
+                        "                   >>:5%!|v         <    >91+,:!|\n" +
+                        "                               >$        ^      \n" +
+                        "                         >>:3%!|    >$   ^ \n" +
+                        "                               >:5%!|\n" +
+                        "                                    >.   ^            \n" +
+                        "              ^                                 <"));
     }
 }
