30e8252a17a183393ed98b63a18a5bc1749a42e7
SergeyKoavalev <dip.deep@mail.ru>
2022-01-23 00:01:59 +0300

    Added test

---
+++ src/test/java/BefungeInterpreterTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BefungeInterpreterTest {
+
+    @Test
+    public void tests() {
+        assertEquals(
+                "123456789",
+                new BefungeInterpreter().interpret(">987v>.v\nv456<  :\n>321 ^ _@"));
+
+    }
+}
